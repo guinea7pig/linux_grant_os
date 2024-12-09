0026b571a6465d84b7f9cf916d11d7482f8582b8
guinea7pig <145406121+guinea7pig@users.noreply.github.com>
2024-12-09 13:21:24 -0500

    the installl file for grant os

---
+++ grant_os_install.sh
@@ -0,0 +1,26 @@
+#copright 2024 guinea7pig
+#grabs packeges from ypm and installs theme
+#this script assumes that you start with the linux kernal and ypm
+# most of this is placeholder beacus ypm isent mad yet also dont know hot to write ssh
+
+#installs required packegese
+
+#using apt beacuse ypm isnt set up yet
+apt install sudo
+apt install git
+
+#config ypm
+#set up users and and password 
+useradd -d /home/username username #make the user add some type of string tha
+
+passwd username #for user above
+
+sudo passwd root # sets 
+
+
+
+
+#install other packeg
+
+ apt install noefetch 
+
